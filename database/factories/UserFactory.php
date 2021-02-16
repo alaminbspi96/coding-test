6bc65db6ca8ae0182504758c89866c633edd8277
alaminbspi96 <alaminbspi96@gmail.com>
2021-02-16 17:15:44 +0600

    password changes

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -22,7 +22,7 @@ $factory->define(User::class, function (Faker $faker) {
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        'password' => '$2y$12$ZI6hj41tq86r3CdSlHKWUuznZC5BfrWH7nsa9iJ5hkDC9e6jWXueG', // 12345678
         'remember_token' => Str::random(10),
     ];
 });
